ec4a5181501b66ccc77bae791a32d83a4cae6b05
Emilie FRAYSSE <emilie.fraysse@gmail.com>
2025-09-20 17:23:25 +0200

    made more ugly plots and ugly loops (save me)

--- data_exploration_script.R
+++ data_exploration_script.R
@@ -1,4 +1,4 @@
-setwd("C:/Users/emili/Desktop/Better_meteo_data/data_cut")
+setwd("C:/Users/emili/Desktop/Better_meteo_data/data_cut/recent_files")
 
 library(ggplot2)
 library(tidyverse)
@@ -10,8 +10,9 @@ library(dplyr)
 library(lubridate)
 library(stringr)
 library(zoo)
+library(rlang)
 
-dir()[35]
+dir()
 files <- list.files(pattern = "\\.csv$")
 
 data <- data.table::fread(dir()[35])
@@ -25,6 +26,7 @@ nrow_df <- data.frame(file = character(),
                       group = character(),
                       stringsAsFactors = FALSE)
 
+### not useful anymore cause i put separate folders
 for (f in files) {
   message("Processing: ", f)
 
@@ -58,3 +60,75 @@ nrow_df %>% ggplot() +
         title = element_text(size = 20),
         axis.title = element_text(size = 14, face = "bold"),
         legend.text = element_text(size = 14))
+
+
+
+for (f in files) {
+  message("Processing: ", f)
+  dat <- data.table::fread(f)
+
+  file_id <- substr(f, start = 17, stop = 20)
+
+  rr_year <- dat %>% summarise(mean_RR = mean(RR), .by = (year))
+  tm_year <- dat %>% summarise(mean_RR = mean(TM), .by = (year))
+  tx_year <- dat %>% summarise(mean_RR = mean(TX), .by = (year))
+
+  var_year <- rr_year %>%
+    left_join(tm_year, by = "year") %>%
+    left_join(tx_year, by = "year") %>%
+    mutate(file = file_id)
+
+  var_region <- bind_rows(var_region, var_year)
+
+  rm(dat)
+  gc()
+}
+
+var_region <- data.frame()
+for (f in files) {
+  message("Processing: ", f)
+  dat <- data.table::fread(f)
+
+  file_id <- substr(f, start = 17, stop = 20)
+
+  var_year <- dat %>%
+    group_by(year) %>%
+    summarise(
+      mean_RR = mean(RR, na.rm = TRUE),
+      mean_TM = mean(TM, na.rm = TRUE),
+      mean_TX = mean(TX, na.rm = TRUE),
+      .groups = "drop"
+    ) %>%
+    mutate(region = file_id)
+
+  var_region <- bind_rows(var_region, var_year)
+
+  rm(dat)
+  gc()
+}
+colnames(var_region)
+head(var_region)
+nrow(var_region)
+unique(var_region$region)
+
+
+
+var_region %>%
+  ggplot() +
+  aes(x = year) +
+  geom_histogram()
+#  geom_line(x = year, y = rr_year, color = rr_year) +
+#  geom_point(x = year, y = tm_year, color = tm_year) +
+#  geom_point(x = year, y = tx_year, color = tx_year)
+
+## "mean_RR" "mean_TM" "mean_TX"
+
+plotting_per_region <- function(reg, variable) {
+  var_region %>%
+    filter(reg == region) %>%
+    ggplot(aes(x = year, y = .data[[variable]]),
+               colour = as.factor(year)) +
+    geom_point() +
+    geom_line()
+}
+plotting_per_region("_01_", "mean_RR")
