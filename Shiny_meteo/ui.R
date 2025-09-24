fa62e20ac99f6a122d577bbf7bfb7318baf54cc5
Naux Emilie <emilienauxpro@gmail.com>
2025-09-24 16:00:03 +0200

    Mise à jour de server.R et ui.R

--- Shiny_meteo/ui.R
+++ Shiny_meteo/ui.R
@@ -1,33 +1,56 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
 library(shiny)
+library(colourpicker)
 
 # Define UI for application that draws a histogram
-fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
-    )
+shinyUI(
+  # navbarPage
+  navbarPage("Premiers pas avec shiny",
+             
+             # premier onglet Data
+             tabPanel("Data", 
+                      navlistPanel(
+                        widths = c(2, 10), 
+                        tabPanel("Table", dataTableOutput("table")),
+                        tabPanel("Summary",verbatimTextOutput("summary"))
+                      )
+             ), 
+             
+             # second onglet Visualisation
+             tabPanel("Visualisation", 
+                      
+                      fluidRow(
+                        # premier colonne
+                        column(width = 3, 
+                               # wellPanel pour griser
+                               wellPanel(
+                                 sliderInput("bins",
+                                             "Number of bins:",
+                                             min = 1,
+                                             max = 50,
+                                             value = 30),
+                                 
+                                 # input pour la couleur
+                                 colourInput(inputId = "color", label = "Couleur :", value = "purple"),
+                                 
+                                 # titre du graphique
+                                 textInput(inputId = "titre", label = "Titre :", value = "Histogramme"),
+                                 
+                                 # selection de la colonne
+                                 radioButtons(inputId = "var", label = "Variable : ", choices = colnames(faithful))
+                               )
+                        ), 
+                        # deuxieme colonne
+                        column(width = 9, 
+                               tabsetPanel(
+                                 tabPanel("Histogramme", 
+                                          plotOutput("distPlot"),
+                                          # classes (div centrée)
+                                          div(textOutput("n_bins"), align = "center")
+                                 ),
+                                 tabPanel("Boxplot", plotOutput("boxplot"))
+                               )
+                        )
+                      )
+             )
+  )
 )
